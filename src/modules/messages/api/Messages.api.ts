7b10df7812fbac5f9071a5f97926ad9fb00e89b8
Daniil Lohvinov <32868304+dlohvinov@users.noreply.github.com>
2026-03-23 14:20:56 +0200

    refactor: update message history service to use transformed camelCase <-> snake_case parameters and response types [WMSG-44](https://webitel.atlassian.net/browse/WMSG-44)

--- src/modules/messages/api/Messages.api.ts
+++ src/modules/messages/api/Messages.api.ts
@@ -1,8 +1,7 @@
+import { applyTransform, snakeToCamel, camelToSnake } from '@webitel/api-services/api/transformers';
+
+import type { MessageHistorySearchParams, MessageHistorySearchRawResponse } from '../types/Message.types';
 import type { ServiceConfig } from '../../configs';
-import type {
-    MessageHistorySearchThreadMessagesHistoryWebitelImApiGatewayV1MessageHistoryParams,
-    WebitelImApiGatewayV1SearchMessageHistoryResponse,
-} from '@webitel/api-services/gen/models';
 
 /**
  * Message history for a thread: `GET /v1/{threadId}/messages`
@@ -12,10 +11,16 @@ export const getMessagesService = ({ axiosInstance }: ServiceConfig) => {
     return {
         getMessageHistory: async (
             threadId: string,
-            params: MessageHistorySearchThreadMessagesHistoryWebitelImApiGatewayV1MessageHistoryParams = {},
-        ): Promise<WebitelImApiGatewayV1SearchMessageHistoryResponse> => {
-            const response = await axiosInstance.get(`/v1/${threadId}/messages`, { params });
-            return response.data;
+            params: MessageHistorySearchParams = {},
+        ): Promise<MessageHistorySearchRawResponse> => {
+            const transformedParams = applyTransform(params, [
+                camelToSnake(),
+            ]);
+
+            const response = await axiosInstance.get(`/v1/${threadId}/messages`, { params: transformedParams });
+            return applyTransform(response.data, [
+                snakeToCamel(),
+            ]);
         },
     };
 };
--- src/modules/messages/types/Message.types.ts
+++ src/modules/messages/types/Message.types.ts
@@ -19,5 +19,6 @@ export type {
     MessageModel,
     IMessage,
     MessageHistorySearchParams,
+    MessageHistorySearchRawResponse,
     MessageHistorySearchResult,
 };
